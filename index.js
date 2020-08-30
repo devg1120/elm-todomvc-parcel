fb4ce4576414d8c8cc2b849bd47ca7b24ce45eb6
devg1120 <devg1120@gmail.com>
2020-08-30 18:36:48 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Elm } = require("./src/Main.elm");
+
+
+var storedState = localStorage.getItem('elm-todo-save');
+var startingState = storedState ? JSON.parse(storedState) : null;
+var app = Elm.Main.init({ flags: startingState });
+app.ports.setStorage.subscribe(function(state) {
+    localStorage.setItem('elm-todo-save', JSON.stringify(state));
+});
